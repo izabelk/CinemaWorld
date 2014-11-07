d8f4eb169ba1cab2aadcb23de5894f04308ee0dd
izabelk <izi_girl@abv.bg>
2014-11-07 12:39:48 +0200

    Added skeleton of the project

---
+++ CinemaWorldProject/CinemaWorld.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CinemaWorld.Web.MvcApplication" Language="C#" %>
---
+++ CinemaWorldProject/CinemaWorld.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CinemaWorld.Web.Startup))]
+namespace CinemaWorld.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
