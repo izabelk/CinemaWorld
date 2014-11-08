781a377cd42b913a5c9dd2ea7a2c8dd6fe7f73aa
izabelk <izi_girl@abv.bg>
2014-11-08 16:35:25 +0200

    Installedn Kendo

--- CinemaWorldProject/CinemaWorld.Web/App_Start/BundleConfig.cs
+++ CinemaWorldProject/CinemaWorld.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,11 @@ namespace CinemaWorld.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/Kendo/kendo.all.min.js",
+                        "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -26,6 +31,12 @@ namespace CinemaWorld.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+                    "~/Content/Kendo/kendo.common-bootstrap.min.css",
+                    "~/Content/Kendo/kendo.bootstrap.min.css"));
+
+            bundles.IgnoreList.Clear();
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
