59235ed52ef47d424fe2fa0b26f459ebf86fac1f
izabelk <izi_girl@abv.bg>
2014-11-13 19:59:40 +0200

    Fixed dropdown  menu and added News Controller to test nested Layout page

--- CinemaWorldProject/CinemaWorld.Web/App_Start/BundleConfig.cs
+++ CinemaWorldProject/CinemaWorld.Web/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@
 
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/unobtrusive-jquery").Include(
@@ -21,8 +20,6 @@
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -31,8 +28,10 @@
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.cerulean.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.cerulean.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include(
+                       "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
                     "~/Content/Kendo/kendo.common-bootstrap.min.css",
@@ -40,8 +39,6 @@
 
             bundles.IgnoreList.Clear();
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = false;
         }
     }
--- CinemaWorldProject/CinemaWorld.Web/Areas/Administration/Controllers/NewsController.cs
+++ CinemaWorldProject/CinemaWorld.Web/Areas/Administration/Controllers/NewsController.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using CinemaWorld.Data.UnitOfWork;
+using CinemaWorld.Web.Areas.Administration.Controllers.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CinemaWorld.Web.Areas.Administration.Controllers
 {
-    public class NewsController
+    public class NewsController : AdminController
     {
+        public NewsController(ICinemaWorldData data) : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
