93f25d842dd809f29d28b18e1115fe618a5c1ebc
izabelk <izi_girl@abv.bg>
2014-11-14 01:49:12 +0200

    Added admnisitration for news, should be fixed with date formats!

--- CinemaWorldProject/CinemaWorld.Web/Areas/Administration/Controllers/NewsController.cs
+++ CinemaWorldProject/CinemaWorld.Web/Areas/Administration/Controllers/NewsController.cs
@@ -1,22 +1,80 @@
-﻿using CinemaWorld.Data.UnitOfWork;
-using CinemaWorld.Web.Areas.Administration.Controllers.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace CinemaWorld.Web.Areas.Administration.Controllers
+﻿namespace CinemaWorld.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.Extensions;
+
+    using AutoMapper;
+
+    using CinemaWorld.Data.UnitOfWork;
+    using CinemaWorld.Models;
+    using CinemaWorld.Web.Areas.Administration.Controllers.Base;
+    using CinemaWorld.Web.Areas.ViewModels.News;
+    
     public class NewsController : AdminController
     {
         public NewsController(ICinemaWorldData data) : base(data)
         {
         }
 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var news = this.Data
+                .News
+                .All()
+                .ToDataSourceResult(request, ModelState);
+
+            return this.Json(news);
+        }
+
+        [HttpPost]
+        public ActionResult Create([DataSourceRequest]DataSourceRequest request, NewsViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var dbModel = Mapper.Map<News>(model);
+                this.Data.News.Add(dbModel);
+                this.Data.SaveChanges();
+                model.Id = dbModel.Id;
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, NewsViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var news = this.Data.News.All().FirstOrDefault(n => n.Id == model.Id);
+                news.Content = model.Content;
+                news.CreatedOn = model.CreatedOn;
+                //Mapper.Map(model, news);
+                this.Data.SaveChanges();
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, NewsViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var dbModel = Mapper.Map<News>(model);
+                this.Data.News.Delete(dbModel);
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
         }
     }
 }
---
+++ CinemaWorldProject/CinemaWorld.Web/Areas/ViewModels/News/NewsViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CinemaWorld.Web.Areas.ViewModels.News
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    using CinemaWorld.Models;
+    using CinemaWorld.Web.Infrastructure.Mapping;
+
+    public class NewsViewModel : IMapFrom<News>
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Posted on")]
+        public DateTime CreatedOn { get; set; }
+    }
+}
