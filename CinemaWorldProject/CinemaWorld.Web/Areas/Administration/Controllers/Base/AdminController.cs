452e1934b663b12702ec11da045802b9e3518b23
izabelk <izi_girl@abv.bg>
2014-11-13 19:18:09 +0200

    Added Admin Controller

---
+++ CinemaWorldProject/CinemaWorld.Web/Areas/Administration/Controllers/Base/AdminController.cs
@@ -0,0 +1,16 @@
+﻿namespace CinemaWorld.Web.Areas.Administration.Controllers.Base
+{
+    using System.Web.Mvc;
+
+    using CinemaWorld.Data.UnitOfWork;
+    using CinemaWorld.Web.Controllers;
+
+    [Authorize(Roles = "Admin")]
+    public abstract class AdminController : BaseController
+    {
+        public AdminController(ICinemaWorldData data)
+            : base(data)
+        {
+        }
+    }
+}
