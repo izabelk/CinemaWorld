a3f69079f0eeab9dff3057d3293a164b9251e7ea
izabelk <izi_girl@abv.bg>
2014-11-15 15:16:18 +0200

    Did more search on program

--- CinemaWorldProject/CinemaWorld.Web/Controllers/ProjectionsController.cs
+++ CinemaWorldProject/CinemaWorld.Web/Controllers/ProjectionsController.cs
@@ -10,7 +10,8 @@
 
     using CinemaWorld.Data.UnitOfWork;
     using CinemaWorld.Web.ViewModels.Program;
-   
+    using CinemaWorld.Web.ViewModels.Projection;
+
     public class ProjectionsController : BaseController
     {
         public ProjectionsController(ICinemaWorldData data)
@@ -34,24 +35,78 @@
             return View(projections);
         }
 
-        public ActionResult Search(string date)
+        public JsonResult GetProjectionCityData()
         {
-            var provider = CultureInfo.InvariantCulture;
-            string format = "d";
-            var dateValue = DateTime.ParseExact(date, format, provider);
+            var result = this.Data.Cities
+                .All()
+                .Select(c => new
+                {
+                    City = c.Name
+                });
 
-            var projections = this
-               .Data
-               .Projections
-               .All()
-               .Where(p => p.ShownOn.Day == dateValue.Day &&
-                   p.ShownOn.Year == dateValue.Year &&
-                   p.ShownOn.Month == p.ShownOn.Month)
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetProjectionCinemaData()
+        {
+            var result = this.Data.Cinemas
+                .All()
+                .Select(c => new
+                {
+                    Cinema = c.Name
+                });
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetProjectionMovieData(string Movie)
+        {
+            var result = this.Data.Movies
+                  .All()
+                  .Select(m => new
+                  {
+                      Movie = m.Title
+                  });
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Search(SubmitProjectionSearchViewModel model)
+        {
+            var projections = this.Data.Projections.All();
+
+            if (model.Date != null)
+            {
+                var provider = CultureInfo.InvariantCulture;
+                string format = "d";
+                var dateValue = DateTime.ParseExact(model.Date, format, provider);
+                projections = projections
+                    .Where(p => p.ShownOn.Day == dateValue.Day &&
+                    p.ShownOn.Year == dateValue.Year &&
+                    p.ShownOn.Month == p.ShownOn.Month);
+            }
+
+            if (model.CinemaName != "All")
+            {
+                projections = projections.Where(p => p.Hall.Cinema.Name == model.CinemaName);
+            }
+
+            if (model.CityName != "All")
+            {
+                projections = projections.Where(p => p.Hall.Cinema.City.Name == model.CityName);
+            }
+
+            if (model.MovieName != "All")
+            {
+                projections = projections.Where(p => p.Movie.Title == model.MovieName);
+            }
+
+            var endResult = projections
                .Project().To<ProjectionViewModel>()
                .GroupBy(p => p.Movie)
                .ToDictionary(p => p.Key);
 
-            return this.PartialView("_ProgramResult", projections);
+            return this.PartialView("_ProgramResult", endResult);
         }
     }
 }
---
+++ CinemaWorldProject/CinemaWorld.Web/ViewModels/Projection/SubmitProjectionSearchViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace CinemaWorld.Web.ViewModels.Projection
+{
+    using AutoMapper;
+
+    using CinemaWorld.Models;
+    using CinemaWorld.Web.Infrastructure.Mapping;
+
+    public class SubmitProjectionSearchViewModel : IMapFrom<Projection>, IHaveCustomMappings
+    {
+        public string Date { get; set; }
+
+        public string MovieName { get; set; }
+
+        public string CinemaName { get; set; }
+
+        public string CityName { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Projection, SubmitProjectionSearchViewModel>()
+              .ForMember(m => m.MovieName, opt => opt.MapFrom(u => u.Movie.Title));
+
+            configuration.CreateMap<Projection, SubmitProjectionSearchViewModel>()
+              .ForMember(m => m.CinemaName, opt => opt.MapFrom(u => u.Movie.Duration));
+
+            configuration.CreateMap<Projection, SubmitProjectionSearchViewModel>()
+                .ForMember(m => m.CityName, opt => opt.MapFrom(u => u.Hall.Cinema.Name));
+        }
+    }
+}
